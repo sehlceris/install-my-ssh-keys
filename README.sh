cd8d7ce575d039d1f20f691a5b7b07cdcda0f6a2
Chris L <sehlceris@local>
2024-09-04 00:39:17 -0700

    initial commit

---
+++ README.sh
@@ -0,0 +1,8 @@
+# Install My SSH Keys
+
+This shell script installs my SSH keys into the current user's `~/.ssh/authorized_keys` file,
+granting me access to SSH into the user.
+
+```shell
+TODO
+```
